753b8d534ea27a201a1fe4237126424f812a5840
maeybilisim <36985268+maeybilisim@users.noreply.github.com>
2018-03-02 17:44:02 +0300

    Add files via upload

---
+++ ogrenciler.js
@@ -0,0 +1,59 @@
+{"ogrenciler":[
+
+   {
+        "ogrenciNo":"1",
+		"ogrenciAdi":"Adı",
+		"ogrenciSoyad":"Soyadı",
+		"bolumAdi":"Bilgi İşlem Mühendisliği",
+		"okulAdi":"Elginkan Valfsel",
+		"ogrenciSinif":"3",
+		"ogrenciResim":""
+		},
+		{
+		"ogrenciNo":"2",
+		"ogrenciAdi":"Adı",
+		"ogrenciSoyad":"Soyadı",
+		"bolumAdi":"Bilgi İşlem Mühendisliği",
+		"okulAdi":"Elginkan Valfsel",
+		"ogrenciSinif":"4",
+		"ogrenciResim":""
+		},
+		{
+		"ogrenciNo":"3",
+		"ogrenciAdi":"Adı",
+		"ogrenciSoyad":"Soyadı",
+		"bolumAdi":"Bilgi İşlem Mühendisliği",
+		"okulAdi":"Elginkan Valfsel",
+		"ogrenciSinif":"2",
+		"ogrenciResim":""
+		},
+		{
+		"ogrenciNo":"4",
+		"ogrenciAdi":"Adı",
+		"ogrenciSoyad":"Soyadı",
+		"bolumAdi":"Bilgi İşlem Mühendisliği",
+		"okulAdi":"Elginkan Valfsel",
+		"ogrenciSinif":"2",
+		"ogrenciResim":""
+		},
+		{
+		"ogrenciNo":"5",
+		"ogrenciAdi":"Adı",
+		"ogrenciSoyad":"Soyadı",
+		"bolumAdi":"Arge Mühendisliği",
+		"okulAdi":"Elginkan Valfsel",
+		"ogrenciSinif":"1",
+		"ogrenciResim":""
+		},
+		
+		{
+		"ogrenciNo":"6",
+		"ogrenciAdi":"Adı",
+		"ogrenciSoyad":"Soyadı",
+		"bolumAdi":"Bilgi İşlem Mühendisliği",
+		"okulAdiAdi":"Elginkan Valfsel",
+		"ogrenciSinif":"1",
+		"ogrenciResim":""
+		}
+		]}
+		
